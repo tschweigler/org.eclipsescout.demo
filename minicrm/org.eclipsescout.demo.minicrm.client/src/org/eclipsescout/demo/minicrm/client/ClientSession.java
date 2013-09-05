49e43703dbc6af662851caaaf8350bc3df4e67b8
Thomas Schweigler <thomas.schweigler@bsiag.com>
2013-09-05 11:14:31 +0200

    Anpassung der Serverurl

--- minicrm/org.eclipsescout.demo.minicrm.client/src/org/eclipsescout/demo/minicrm/client/ClientSession.java
+++ minicrm/org.eclipsescout.demo.minicrm.client/src/org/eclipsescout/demo/minicrm/client/ClientSession.java
@@ -4,11 +4,13 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
 package org.eclipsescout.demo.minicrm.client;
+
+import java.io.Serializable;
 
 import org.eclipse.scout.commons.exception.ProcessingException;
 import org.eclipse.scout.commons.logger.IScoutLogger;
@@ -19,7 +21,8 @@ import org.eclipse.scout.rt.client.servicetunnel.http.HttpServiceTunnel;
 import org.eclipse.scout.rt.shared.services.common.code.CODES;
 import org.eclipsescout.demo.minicrm.client.ui.desktop.Desktop;
 
-public class ClientSession extends AbstractClientSession {
+public class ClientSession extends AbstractClientSession implements Serializable {
+  private static final long serialVersionUID = 1L;
   private static IScoutLogger logger = ScoutLogManager.getLogger(ClientSession.class);
 
   public ClientSession() {
