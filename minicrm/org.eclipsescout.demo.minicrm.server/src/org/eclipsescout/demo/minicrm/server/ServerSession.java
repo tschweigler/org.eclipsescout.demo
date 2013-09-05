4cab433ca91e6981671568296e5b75ffc1ed92b1
Thomas Schweigler <thomas.schweigler@bsiag.com>
2013-09-05 08:10:59 +0200

    Anpassung Server URL Einbinden von MySQL

--- minicrm/org.eclipsescout.demo.minicrm.server/src/org/eclipsescout/demo/minicrm/server/ServerSession.java
+++ minicrm/org.eclipsescout.demo.minicrm.server/src/org/eclipsescout/demo/minicrm/server/ServerSession.java
@@ -4,11 +4,13 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
 package org.eclipsescout.demo.minicrm.server;
+
+import java.io.Serializable;
 
 import org.eclipse.scout.commons.exception.ProcessingException;
 import org.eclipse.scout.commons.logger.IScoutLogger;
@@ -16,7 +18,8 @@ import org.eclipse.scout.commons.logger.ScoutLogManager;
 import org.eclipse.scout.rt.server.AbstractServerSession;
 import org.eclipse.scout.rt.server.ServerJob;
 
-public class ServerSession extends AbstractServerSession {
+public class ServerSession extends AbstractServerSession implements Serializable {
+  private static final long serialVersionUID = 1L;
   private static IScoutLogger logger = ScoutLogManager.getLogger(ServerSession.class);
 
   public ServerSession() {
