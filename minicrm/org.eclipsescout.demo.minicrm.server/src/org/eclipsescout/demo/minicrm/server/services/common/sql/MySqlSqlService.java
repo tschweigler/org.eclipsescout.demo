8250e70c9691904ce50df8a99314957b48b07efd
Thomas Schweigler <thomas.schweigler@bsiag.com>
2014-03-04 18:17:59 +0100

    Test

--- minicrm/org.eclipsescout.demo.minicrm.server/src/org/eclipsescout/demo/minicrm/server/services/common/sql/MySqlSqlService.java
+++ minicrm/org.eclipsescout.demo.minicrm.server/src/org/eclipsescout/demo/minicrm/server/services/common/sql/MySqlSqlService.java
@@ -5,21 +5,40 @@ import org.eclipse.scout.service.IService;
 import com.bsiag.scout.rt.server.jdbc.AbstractMySqlSqlService;
 
 public class MySqlSqlService extends AbstractMySqlSqlService implements IService {
-  @Override
-  protected String getConfiguredJdbcMappingName() {
-    //return "jdbc:mysql://ec2-23-21-211-172.compute-1.amazonaws.com:3306/minicrm";
-    return "jdbc:mysql://localhost:3306/minicrm";
-  }
 
-  @Override
-  protected String getConfiguredPassword() {
-    //return "minicrm";
-    return "";
-  }
+//  private String m_jdbcMappingName;
+//  private String m_username;
+//  private String m_password;
+//
+//  public void setJdbcMappingName(String jdbcMappingName) {
+//    m_jdbcMappingName = jdbcMappingName;
+//  }
+//
+//  public void setUsername(String username) {
+//    m_username = username;
+//  }
+//
+//  public void setPassword(String password) {
+//    m_password = password;
+//  }
 
-  @Override
-  protected String getConfiguredUsername() {
-    //return "minicrm";
-    return "root";
-  }
+//  @Override
+//  protected String getConfiguredJdbcMappingName() {
+//    //return "jdbc:mysql://ec2-23-21-211-172.compute-1.amazonaws.com:3306/minicrm";
+//    return "jdbc:mysql://localhost:3306/minicrm";
+//  }
+//
+//  @Override
+//  protected String getConfiguredPassword() {
+//    //return "minicrm";
+//    //return "";
+//    return "awsdbpassword";
+//  }
+//
+//  @Override
+//  protected String getConfiguredUsername() {
+//    //return "minicrm";
+//    //return "root";
+//    return "awsdbuser";
+//  }
 }
